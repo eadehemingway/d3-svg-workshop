2f0a06bec8c0154e5e76ddba193832126984b92d
eadehemingway <eade.hemingway@gmail.com>
2019-07-17 15:33:20 +0300

    add gif

---
+++ app.js
@@ -0,0 +1,29 @@
+const svgWidth = 800;
+const svgHeight = 400;
+
+const svg = d3
+  .select('#chart')
+  .append('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight);
+
+const bottomLine = 300;
+const bottomLineWidth = 5;
+
+svg
+  .append('line')
+  .attr('x1', 0)
+  .attr('x2', svgWidth)
+  .attr('y1', bottomLine)
+  .attr('y2', bottomLine)
+  .attr('stroke', 'black')
+  .attr('stroke-width', bottomLineWidth);
+
+const circleX = 200;
+const circleRadius = 30;
+svg
+  .append('circle')
+  .attr('cx', circleX)
+  .attr('cy', bottomLine - circleRadius - bottomLineWidth / 2)
+  .attr('r', circleRadius)
+  .attr('fill', 'pink');
